a0d3403ac65983148e0b1beb73f555062634fca8
Sergej Andrejev <sergej.andrejev@embl.de>
2019-03-12 21:15:37 +0100

    add code and data for E. rectale growth on spent media under duloxetine condition

---
+++ exp7spentmedia_Erectale_growth.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(readr)
+
+exp7spentmedia.Erectale_growth = function()
+{
+  bug_map = readr::read_delim("data/bug_map.tsv", "\t")
+  data = readr::read_delim("data/exp7spentmedia_Erectale_growth/data.tsv", "\t", col_types=readr::cols(Annotation=readr::col_character()))  %>%
+    dplyr::group_by(Plate, Well) %>% 
+    dplyr::mutate(OD_corrected=OD578-OD578[Hour==0]) %>%
+    dplyr::left_join(bug_map, by="species.code") %>%
+    dplyr::arrange(match(species.code, c("Ctrl", "Bt", "Ss")), match(Condition, c("fresh", "conditioned", "spent"))) %>%
+    dplyr::mutate(species.short=ifelse(is.na(species.short), "Control", species.short), Group=paste0(species.short, "\n", Condition))
+  data$Group = factor(data$Group, unique(data$Group))
+  
+  data.24 = data %>% dplyr::filter(Hour==24 & Concentration %in% c(0, 50) & BugsGrow=="Er")
+  
+  pdf(file="reports/exp7spentmedia_Erectale_growth.pdf", width=9, height=6)
+  ggplot(data.24, aes(y=OD_corrected, x=Group, fill=as.factor(Concentration))) +
+    geom_boxplot() +
+    geom_point(position=position_jitterdodge()) +
+    scale_fill_manual(values=c("grey","firebrick4")) +
+    labs(x="", y="Baseline corrected OD578",fill="Duloxetine \nconcentration") +
+    facet_wrap(~BugsGrow) +
+    myTheme
+  dev.off()
+}
